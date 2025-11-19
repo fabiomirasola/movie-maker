0ef087a9173ceb0d86af1e99489b9393b536aaa3
fabiomirasola <mirasolafabio@hotmail.com>
2025-11-19 19:39:17 +0100

    Ajout du favoris

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { Movie } from '../../../assets/movie';
 
 @Component({
   selector: 'app-movie-card',
@@ -15,6 +16,25 @@ export class MovieCardComponent {
 
   get posterUrl(): string {
     return this.movie?.Poster && this.movie.Poster !== "N/A" ? this.movie.Poster : this.fallback;
+  }
+
+  isFavorite(movie: Movie): boolean {
+    const favorites: Movie[] = JSON.parse(localStorage.getItem('favoris') || '[]');
+    return favorites.some(f => f.imdbID === movie.imdbID);
+  }
+
+  toggleFavorite(movie: Movie) {
+    let favorites: Movie[] = JSON.parse(localStorage.getItem('favoris') || '[]');
+
+    const index = favorites.findIndex((f: Movie) => f.imdbID === movie.imdbID);
+
+    if (index > -1) {
+      favorites.splice(index, 1); 
+    } else {
+      favorites.push(movie);
+    }
+
+    localStorage.setItem('favoris', JSON.stringify(favorites));
   }
 }
 
